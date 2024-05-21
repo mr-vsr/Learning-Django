ec6374dcfe30a9ae265f7ca58fccebb3e869aabb
mr-vsr <vikasraivsr630@gmail.com>
2024-05-21 21:41:54 +0530

    added hot reload for tailwind

--- learningDjango/learningDjango/settings.py
+++ learningDjango/learningDjango/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'learn',
     'tailwind',
     'theme',
+    'django_browser_reload',
 ]
 
 TAILWIND_APP_NAME = 'theme'
@@ -56,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'learningDjango.urls'
